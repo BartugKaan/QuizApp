264261b5debaab8fa6d029880e3615fd420dfc30
BartugKaan <bartugkaan.celebi@outlook.com>
2023-03-27 20:54:47 +0300

    Added : View connected to Controller

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var highscoreLabel: UILabel!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var answer1Button: UIButton!
+    
+    @IBOutlet weak var answer2Button: UIButton!
+    
+    @IBOutlet weak var answer3Button: UIButton!
+    
+    @IBOutlet weak var progresView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
